59fff270d454bc9d0e23b72e4c8bebff443124c1
Kateryna Borysenko <k.borysenko.kyiv@gmail.com>
2022-07-28 19:12:34 +0300

    Rewrited Profile component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,58 +1,55 @@
-import React, { Component } from "react";
 import Header from "../common/Header/Header";
 import s from "./Profile.module.css";
 import image from "../../images/brain.png";
 
-class Profile extends Component {
-  render() {
-    const {
-      name,
-      surname,
-      birthday,
-      phone,
-      website,
-      about,
-      technologies,
-      project,
-    } = this.props.data;
+const Profile = ({ data }) => {
+  const {
+    name,
+    surname,
+    birthday,
+    phone,
+    website,
+    about,
+    technologies,
+    project,
+  } = data;
 
-    return (
-      <>
-        <div className={s.wrap}>
-          <Header text={`${name} ${surname}`} />
-          <img src={image} className={s.image} alt="brain" />
-          <table>
-            <tbody>
-              <tr>
-                <td>Дата рождения</td>
-                <td>{birthday}</td>
-              </tr>
-              <tr>
-                <td>Телефон</td>
-                <td>{phone}</td>
-              </tr>
-              <tr>
-                <td>Сайт</td>
-                <td>{website}</td>
-              </tr>
-              <tr>
-                <td>О себе</td>
-                <td>{about}</td>
-              </tr>
-              <tr>
-                <td>Стек технологий</td>
-                <td>{technologies}</td>
-              </tr>
-              <tr>
-                <td>Описание последнего проекта</td>
-                <td>{project}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <div className={s.wrap}>
+        <Header text={`${name} ${surname}`} />
+        <img src={image} className={s.image} alt="brain" />
+        <table>
+          <tbody>
+            <tr>
+              <td>Дата рождения</td>
+              <td>{birthday}</td>
+            </tr>
+            <tr>
+              <td>Телефон</td>
+              <td>{phone}</td>
+            </tr>
+            <tr>
+              <td>Сайт</td>
+              <td>{website}</td>
+            </tr>
+            <tr>
+              <td>О себе</td>
+              <td>{about}</td>
+            </tr>
+            <tr>
+              <td>Стек технологий</td>
+              <td>{technologies}</td>
+            </tr>
+            <tr>
+              <td>Описание последнего проекта</td>
+              <td>{project}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
 
 export default Profile;
